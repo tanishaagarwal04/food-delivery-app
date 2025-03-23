2dfaddec35fc1dad0fd11ab52f986c4502af994e
Tanisha Agarwal <tanisha.ag2004@gmail.com>
2025-03-23 18:13:58 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Cart from "./pages/Cart/Cart";
-import Home from "./pages/Home/home";
+import Home from "./pages/Home/Home";
 import { Route, Routes } from "react-router-dom";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
